36b85aea6e51071d3e8364b17d5bb323d4a0992c
Heather Zurek <heather.zurek@gmail.com>
2019-05-03 13:07:44 -0700

    fix models

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -1,5 +1,29 @@
+// using Microsoft.AspNetCore.Mvc;
+// using WordCounter.Models;
+//
+// namespace WordCounter.Controllers
+// {
+//   public class HomeController : Controller
+//   {
+//     [HttpGet("/")]
+//     public ActionResult Index()
+//     {
+//       return View(0);
+//     }
+//
+//     [HttpPost("/wordcounter/new")]
+//     public ActionResult New()
+//     {
+//       return View();
+//     }
+//
+//   }
+// }
+
 using Microsoft.AspNetCore.Mvc;
 using WordCounter.Models;
+using System.Collections.Generic;
+
 
 namespace WordCounter.Controllers
 {
@@ -11,11 +35,19 @@ namespace WordCounter.Controllers
       return View(0);
     }
 
-    [HttpPost("/wordcounter/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+    // [HttpGet("/wordcounter/new")]
+    // public ActionResult New()
+    // {
+    //   return View();
+    // }
+    //
+    // [HttpPost("/wordcounter")]
+    // public ActionResult Create(string makemodel, int price, int miles)
+    // {
+    //   Word findWord = new Word(_findWord);
+    //   return RedirectToAction("Index");
+    // }
+
 
   }
 }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -1,83 +1,84 @@
-namespace WordCounter.Models
-{
-  public class Word
-  {
-    private string _findWord;
-    private int _wordCount = 0;
-
-
-    public Word (string findWord)
-    {
-      _findWord = findWord;
-    }
-
-    public string GetFindWord()
-    {
-      return _findWord;
-    }
-    //added word count getter
-    public int GetWordCount()
-    {
-      return _wordCount;
-    }
-    //added word count getter
-
-    public void SetFindWord(string newFindWord)
-    {
-      _findWord = newFindWord;
-    }
-
-    public int SearchForWord(string userInput)
-    {
-      // int _wordCount = 0;
-      string searchForWord = userInput;
-      string[] searchArray = searchForWord.Split(' ');
-
-      foreach(string element in searchArray)
-      {
-        if(element == _findWord)
-        {
-          _wordCount ++;
-        }
-
-      }
-      return _wordCount;
-    }
-  }
-}
-
 // namespace WordCounter.Models
 // {
-//   public class RepeatCounter
+//   public class Word
 //   {
-//     private string _word;
-//     private string _phrase;
-//     private string[] _splitPhrase;
+//     private string _findWord;
+//     private int _wordCount = 0;
 //
-//     public RepeatCounter (string word, string phrase)
+//
+//     public Word (string findWord)
 //     {
-//       _word = word;
-//       _phrase = phrase;
-//       _splitPhrase = phrase.Split(" ");
+//       _findWord = findWord;
 //     }
 //
-//     public string Word { get => _word; set => _word = value; }
-//     public string Phrase { get => _phrase; set => _phrase = value; }
-//
-//     public int Counter()
+//     public string GetFindWord()
 //     {
-//       int counter = 0;
+//       return _findWord;
+//     }
+//     //added word count getter
+//     public int GetWordCount()
+//     {
+//       return _wordCount;
+//     }
+//     //added word count getter
 //
-//       foreach (string word in phrase.Split())
+//     public void SetFindWord(string newFindWord)
+//     {
+//       _findWord = newFindWord;
+//     }
+//
+//     public int SearchForWord(string userInput)
+//     {
+//       // int _wordCount = 0;
+//       string searchForWord = userInput;
+//       string[] searchArray = searchForWord.Split(' ');
+//
+//       foreach(string element in searchArray)
 //       {
-//         if (word == phrase.Split())
+//         if(element == _findWord)
 //         {
-//           counter ++;
+//           _wordCouWnt ++;
 //         }
+//
 //       }
-//       return counter;
+//       return _wordCount;
 //     }
-//
-//
 //   }
 // }
+
+namespace WordCounter.Models
+{
+  public class RepeatCounter
+  {
+    private string _word;
+    private string _phrase;
+    // private string[] _splitPhrase;
+
+    public RepeatCounter (string word, string phrase)
+    {
+      _word = word;
+      _phrase = phrase;
+      // _splitPhrase = phrase.Split(" ");
+    }
+
+    public string Word { get => _word; set => _word = value; }
+    public string Phrase { get => _phrase; set => _phrase = value; }
+
+    public int Counter(string userInput)
+    {
+      int counter = 0;
+      string phrase = userInput;
+      string[] userArray = phrase.Split(" ");
+
+      foreach (string word in userArray)
+      {
+        if (word == _word)
+        {
+          counter ++;
+        }
+      }
+      return counter;
+    }
+
+  }
+}
