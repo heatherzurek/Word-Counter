70b07c64bcf4eeed967d3b628033855e985ef348
Heather Zurek <heather.zurek@gmail.com>
2019-05-05 21:19:27 -0700

    fix controller

--- WordCounter.Tests/ControllerTests/HomeControllerTests.cs
+++ WordCounter.Tests/ControllerTests/HomeControllerTests.cs
@@ -22,5 +22,7 @@ namespace WordCounter.Tests
       Assert.IsInstanceOfType(indexView, typeof(ViewResult));
     }
 
+
+
   }
 }
--- WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
+++ WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
@@ -10,18 +10,21 @@ namespace WordCounter.Tests
   public class WordCounterControllerTest
   {
 
-    // [TestMethod]
-    // public void Index_HasCorrectModelType_ItemList()
-    // {
-    //   //Arrange
-    //   ViewResult indexView = new HomeController().Index() as ViewResult;
-    //
-    //   //Act
-    //   var result = indexView.ViewData.Model;
-    //
-    //   //Assert
-    //   Assert.IsInstanceOfType(result, typeof(List<Word>));
-    // }
+    [TestClass]
+    public class WordCounterControllerTest
+    {
+      [TestMethod]
+      public void Index_ReturnsCorrectView_True()
+      {
+        //Arrange
+        WordCounterController controller = new WordCounterController();
+
+        //Act
+        ActionResult indexView = controller.Index();
+
+        //Assert
+        Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+      }
 
   }
 }
